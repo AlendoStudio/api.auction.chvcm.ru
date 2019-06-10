3bcf47d245d3a6726ff87e5bb09d055daf0ff5e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-10 11:02:32 +0000

    Deploy AlendoStudio/api.auction.chvcm.ru to github.com/AlendoStudio/api.auction.chvcm.ru.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "Интернет-аукцион для юридических лиц",
+  "version": "1.0.0",
+  "description": "Документация RESTful и WebSocket API",
+  "url": "/api/v1",
+  "template": {
+    "forceLanguage": "ru"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-06-10T11:02:22.406Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
